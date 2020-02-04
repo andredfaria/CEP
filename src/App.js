a0cd06cbe7594549959dbf7e6f41c63d8188a0a6
André <adfariacarvalho@gmail.com>
2020-02-03 23:28:20 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+
 function App() {
+
+  const envia = function(){
+    
+  }
+
   return (
     <div className="App">
       
         <Container>
+          <h1>Busca por CEP</h1>
           <Row>
             <Col>
               <InputGroup className="mb-3">
                 <InputGroup.Prepend>
-                  <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
+                  <InputGroup.Number id="cep">Digite aqui seu CEP</InputGroup.Number>
                 </InputGroup.Prepend>
                 <FormControl
-                  placeholder="Username"
-                  aria-label="Username"
-                  aria-describedby="basic-addon1"
+                  placeholder="CEP"
+                  aria-label="CEP"
+                  aria-describedby="cep"
                 />
               </InputGroup>
 
             </Col>
             <Col>
-              <Button type="button" class="close" data-dismiss="alert" aria-label="Close">
+              <Button type="button" 
+                      class="btn btn-success" 
+                      data-dismiss="alert" 
+                      onClick={envia()}>
                 <span aria-hidden="true">&times;</span>
               </Button>
             </Col>
