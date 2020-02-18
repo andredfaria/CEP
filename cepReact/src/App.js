04d7e6ffe8c5bac47c1552f71e31578dfbbef6b8
André de Faria <andredefaria_@hotmail.com>
2020-02-18 01:04:46 -0300

    inicialização do React no projeto

--- cepReact/src/App.js
+++ cepReact/src/App.js
@@ -1,49 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
-import { Button, Container, Row, Col, InputGroup, FormControl } from 'react-bootstrap';
-
-import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-
 function App() {
-
-  const envia = function(){
-    
-  }
-
   return (
     <div className="App">
-      
-        <Container>
-          <h1>Busca por CEP</h1>
-          <Row>
-            <Col>
-              <InputGroup className="mb-3">
-                <InputGroup.Prepend>
-                  <InputGroup.Number id="cep">Digite aqui seu CEP</InputGroup.Number>
-                </InputGroup.Prepend>
-                <FormControl
-                  placeholder="CEP"
-                  aria-label="CEP"
-                  aria-describedby="cep"
-                />
-              </InputGroup>
-
-            </Col>
-            <Col>
-              <Button type="button" 
-                      class="btn btn-success" 
-                      data-dismiss="alert" 
-                      onClick={envia()}>
-                <span aria-hidden="true">&times;</span>
-              </Button>
-            </Col>
-
-          </Row>
-        </Container>
-
-
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
