8f95d3fc89fb8c07b140fd015339e1f2beba207e
André de Faria <andredefaria_@hotmail.com>
2020-02-21 00:27:45 -0300

    get dados

--- cepJQuery/controller.js
+++ cepJQuery/controller.js
@@ -1,13 +1,72 @@
+$(function(){
+    // $.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/", function(resultado){
+    //     resultado.forEach(element => {     
+    //         $('#selectEstados').append($('<option>', {
+    //             value: element.id,
+    //             text: element.nome
+    //         }));
+    //     });
+    // });
+    $.get("https://servicodados.ibge.gov.br/api/v1/localidades/regioes", function(resultado){
+        resultado.forEach(element => {     
+            $('#selectRegioes').append($('<option>', {
+                value: element.id,
+                text: element.nome
+            }));
+        });
+    });
+});
+
 function enviar(){
-    // alert($("#cep").val());
     var cep = $("#cep").val();
     $.get("https://viacep.com.br/ws/"+cep+"/json/", function(resultado){
-          console.log(resultado);
-          $("#mensagem").html(resultado);
 
-          $("#localidade").val(resultado.localidade);
+    $("#localidade").val(resultado.localidade);
           $("#rua").val(resultado.logradouro);
           $("#bairro").val(resultado.bairro);
           $("#estado").val(resultado.uf);
     });
+};
+
+
+function regioes(){
+    var macrorregiao = $('#selectRegioes').children("option:selected").val();
+
+    $.get("https://servicodados.ibge.gov.br/api/v1/localidades/regioes/"+macrorregiao+"/estados", function(resultado){
+        resultado.forEach(element => {    
+            console.log(element);
+            $('#selectEstados').append($('<option>', {
+                value: element.id,
+                text: element.nome
+            }));
+        });
+    });
 }
+
+function estado(){
+    var estado = $('#selectEstados').children("option:selected").val();
+
+    $.get("https://servicodados.ibge.gov.br/api/v1/localidades/estados/"+estado+"/municipios", function(resultado){
+        resultado.forEach(element => {    
+            console.log(element);
+            $('#selectMunicipios').append($('<option>', {
+                value: element.id,
+                text: element.nome
+            }));
+        });
+    });
+}
+
+function municipio(){
+    var municipio = $('#selectMunicipios').children("option:selected").val();
+
+    $.get("https://servicodados.ibge.gov.br/api/v1/localidades/municipios/"+municipio+"/subdistritos", function(resultado){
+        resultado.forEach(element => {     
+            $('#selecSubdistritos').append($('<option>', {
+                value: element.sigla,
+                text: element.nome
+            }));
+        });
+    });
+}
+
